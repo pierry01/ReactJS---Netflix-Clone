50a268e71c1fa1f7fbd71ec25b270683380e1e5f
Jean Mendonça <pierrybm@gmail.com>
2020-09-24 19:30:16 +0000

    Header OK

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ import Header from './components/Header'
 export default () => {
   const [movieList, setMovieList] = useState([])
   const [featuredData, setFeaturedData] = useState(null)
+  const [blackHeader, setBlackHeader] = useState(false)
 
   useEffect(() => {
     const loadAll = async() => {
       let list = await TMDB.getHomeList()
       setMovieList(list)
-      
+
       let originals = list.filter(i => i.slug === 'originals')
       let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
       let chosen = originals[0].items.results[randomChosen]
@@ -25,9 +26,24 @@ export default () => {
     loadAll()
   }, [])
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true)
+      }
+      else {
+        setBlackHeader(false)
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener)
+
+    return () => { window.removeEventListener('scroll', scrollListener) }
+  }, [])
+
   return (
     <div className="page">
-      <Header />
+      <Header black={ blackHeader } />
 
       { 
         featuredData && 
