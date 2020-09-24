891e4002f4839a5caf8d39c0f6aa97ddacee5054
Jean Mendonça <pierrybm@gmail.com>
2020-09-24 18:01:02 +0000

    Data OK

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TMDB from './TMDB'
 
 export default () => {
+  const [movieList, setMovieList] = useState([])
+  
+  useEffect(()=>{
+    const loadAll = async () => {
+      let list = await TMDB.getHomeList()
+      setMovieList(list)
+    }
+    
+    loadAll()
+  }, [])
+
   return (
     <div>
       Olá mundo!!
